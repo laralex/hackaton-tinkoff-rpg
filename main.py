447cd3bca5aaea1c4ad8a4cdfde32441a915c1af
Alex Larionov <alxs.larionov@gmail.com>
2020-09-12 13:35:30 +0300

    Items json

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ Send /start to initiate the conversation.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
-
-from telegram import ReplyKeyboardMarkup
+from functools import wraps
+from telegram import (KeyboardButton, ReplyKeyboardMarkup, ChatAction)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler, PicklePersistence)
 
@@ -41,8 +41,24 @@ def facts_to_str(user_data):
 
     return "\n".join(facts).join(['\n', '\n'])
 
+def send_typing_action(func):
+    """Sends typing action while processing func command."""
 
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+        return func(update, context,  *args, **kwargs)
+
+    return command_func
+
+@send_typing_action
 def start(update, context):
+    # location_keyboard = KeyboardButton(text="send_location", request_location=True)
+    # contact_keyboard = KeyboardButton(text="send_contact", request_contact=True)
+    # custom_keyboard = [[ location_keyboard, contact_keyboard ]]
+    # reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    # update.message.reply_text("Would you mind sharing your location and contact with me?", reply_markup=reply_markup)
+
     reply_text = "Hi! My name is Doctor Botter."
     if context.user_data:
         reply_text += " You already told me your {}. Why don't you tell me something more " \
@@ -51,11 +67,13 @@ def start(update, context):
     else:
         reply_text += " I will hold a more complex conversation with you. Why don't you tell me " \
                       "something about yourself?"
+
+   
     update.message.reply_text(reply_text, reply_markup=markup)
 
     return CHOOSING
 
-
+@send_typing_action
 def regular_choice(update, context):
     text = update.message.text.lower()
     context.user_data['choice'] = text
@@ -68,14 +86,14 @@ def regular_choice(update, context):
 
     return TYPING_REPLY
 
-
+@send_typing_action
 def custom_choice(update, context):
     update.message.reply_text('Alright, please send me the category first, '
                               'for example "Most impressive skill"')
 
     return TYPING_CHOICE
 
-
+@send_typing_action
 def received_information(update, context):
     text = update.message.text
     category = context.user_data['choice']
@@ -90,12 +108,12 @@ def received_information(update, context):
 
     return CHOOSING
 
-
+@send_typing_action
 def show_data(update, context):
     update.message.reply_text("This is what you already told me:"
                               "{}".format(facts_to_str(context.user_data)))
 
-
+@send_typing_action
 def done(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
